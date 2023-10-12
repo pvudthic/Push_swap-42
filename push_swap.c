960bf750bbc626f0841001bee5e8d5160c0777ed
Phanuwat <laernoyats@gmail.com>
2023-10-12 14:42:54 +0700

    start project

---
+++ push_swap.c
@@ -0,0 +1,7 @@
+#include "push_swap.h"
+
+int	main(void)
+{
+	printf("Hello Push_swap");
+	return (0);
+}
---
+++ push_swap.h
@@ -0,0 +1,10 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+/// This should be deleted//
+# include <stdio.h>
+///////////////////////////
+
+
+
+#endif
