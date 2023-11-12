e95c75934c04aee7e9196b081dec473b1af26ac0
Phanuwat <laernoyats@gmail.com>
2023-11-12 23:10:50 +0700

    working stack operation

--- sort_number.c
+++ sort_number.c
@@ -3,6 +3,10 @@
 void	sort(t_list *stack)
 {
 	do_pb(stack);
+	do_sa(stack);
+	do_pb(stack);
+	do_pb(stack);
+	do_sa(stack);
 	do_pa(stack);
 	display_stack(stack);
 	return ;
--- stack_swap_push.c
+++ stack_swap_push.c
@@ -6,53 +6,84 @@ void	do_sa(t_list *stack)
 {
 	int	tmp;
 
-	tmp = stack->a->next->nb;
-	stack->a->next->nb = stack->a->nb;
-	stack->a->nb = tmp;
-	printf("sa\n");
+	if (stack->a && stack->a->next)
+	{
+		tmp = stack->a->next->nb;
+		stack->a->next->nb = stack->a->nb;
+		stack->a->nb = tmp;
+		printf("sa\n");
+	}
+	else
+		return ;
 }
 
 void	do_sb(t_list *stack)
 {
 	int	tmp;
 
-	tmp = stack->b->next->nb;
-	stack->b->next->nb = stack->b->nb;
-	stack->b->nb = tmp;
-	printf("sb\n");
+	if (stack->b && stack->b->next)
+	{
+		tmp = stack->b->next->nb;
+		stack->b->next->nb = stack->b->nb;
+		stack->b->nb = tmp;
+		printf("sb\n");
+	}
+	else
+		return ;
 }
 
 void	do_ss(t_list *stack)
 {
 	int	tmp;
 
-	tmp = stack->a->next->nb;
-	stack->a->next->nb = stack->a->nb;
-	stack->a->nb = tmp;
-	tmp = stack->b->next->nb;
-	stack->b->next->nb = stack->b->nb;
-	stack->b->nb = tmp;
-	printf("ss\n");
+	if (stack)
+	{
+		if (stack->a && stack->a->next)
+		{
+			tmp = stack->a->next->nb;
+			stack->a->next->nb = stack->a->nb;
+			stack->a->nb = tmp;
+		}
+		if (stack->b && stack->b->next)
+		{
+			tmp = stack->b->next->nb;
+			stack->b->next->nb = stack->b->nb;
+			stack->b->nb = tmp;
+		}
+		printf("ss\n");
+	}
+	else
+		return ;
 }
 
 void	do_pa(t_list *stack)
 {
 	t_stack	*tmp_stack;
 
-	tmp_stack = stack->b;
-	stack->b = stack->b->next;
-	tmp_stack->next = stack->a;
-	stack->a = tmp_stack;
-	printf("pa\n");
+	if (stack->b)
+	{
+		tmp_stack = stack->b;
+		stack->b = stack->b->next;
+		tmp_stack->next = stack->a;
+		stack->a = tmp_stack;
+		printf("pa\n");
+	}
+	else
+		return ;
 }
 
 void	do_pb(t_list *stack)
 {
 	t_stack	*tmp_stack;
 
-	tmp_stack = stack->a;
-	stack->a = stack->a->next;
-	tmp_stack->next = stack->b;
-	stack->b = tmp_stack;
-	printf("pb\n");
+	if (stack->a)
+	{
+		tmp_stack = stack->a;
+		stack->a = stack->a->next;
+		tmp_stack->next = stack->b;
+		stack->b = tmp_stack;
+		printf("pb\n");
+	}
+	else
+		return ;
 }
